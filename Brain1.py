df18ad77d27f846e7ff701c19ae78e7f8641a3fe
changjunn <ckdwns2517@g.skku.edu>
2021-11-13 04:29:10 +0900

    ㅇㅇ

--- Brain1.py
+++ Brain1.py
@@ -13,8 +13,9 @@ class Brain1:
         self.prev_trophy_x, self.prev_trophy_y = 0, 0
         self.crossing={1:(250,150),2:(50,400),3:(350,400),4:(250,550),5:(550,400),6:(550,300),7:(700,400),8:(600,500),9:(700,600),10:(950,150)}
         self.previous_pos = [0, 0]
-        self.current_pos = [0, 0]
+        self.current_pos = [0, 0]        
         self.car_direction = 0
+        self.car_speed = 0
         self.lidar = []
         self.respawn_points = [[50, 50], [50, 750], [950, 50], [950, 750]]
         self.startpoint = []    # 현위치, 리스폰점 4개 중 트로피랑 가장 가까운 점
@@ -37,6 +38,9 @@ class Brain1:
                 if self.database.car.position is None:
                     print("while문")
                     continue
+                if self.database.car.speed is None:
+                    print("while문")
+                    continue
                 if 'Trophy' not in self.database.v2x_data.keys():
                     print("while문")
                     continue
@@ -48,6 +52,7 @@ class Brain1:
                     continue
 
                 self.current_pos = self.database.car.position
+                self.car_speed = self.database.car.speed
                 (self.trophy_x, self.trophy_y) = self.database.v2x_data['Trophy']
                 self.car_direction = self.database.car.direction
                 self.lidar = self.database.lidar.data
@@ -58,7 +63,50 @@ class Brain1:
                 self.first_waypoint()
                 self.global_path_planning()
                 # print(self.database.v2x_data['Trophy'])
+
             
+            '''
+            ☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+            ☆☆☆☆☆ DO NOT CHANGE ANOTHER CODE IN 2021-Hackathon-Simulator!!! ☆☆☆☆☆
+            ☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆ ONLY CHANGE Brain.py!!!☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+            ☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+
+            1. How can I get a lidar / gps / imu data?
+                Lidar : data = self.database.lidar.data
+                Gps : data = self.database.car.position
+                IMU : data = self.database.car.direction
+
+            2. How can I move a car?
+                self.database.control.up()
+                self.database.control.down()
+                self.database.control.right()
+                self.database.control.left()
+
+                OR
+
+                self.up(num)
+                self.down(num)
+                self.right(num)
+                self.left(num)
+                ☆☆☆☆☆ num in here is number of acceleration ☆☆☆☆☆
+
+                ☆☆☆☆☆
+                In one loop,
+                you can only change the acceleration up to 5 and the angle up to 8!!
+                Maximum speed of car is 15 and maximum angle of car can rotate is 8!!
+                ☆☆☆☆☆
+
+            3. How can I get a car status data?
+                self.database.car.direction
+                self.database.car.speed
+
+            4. How can I get a v2x data?
+                self.database.v2x_data
+            '''
+
+            # Implement Your Algorithm HERE!!
+
+            # EXAMPLE CODE1: 속도 3으로 유지하면서 오른쪽으로 회전하기
 
 
             # self.curr_dest_index = 0    # 고정
@@ -66,12 +114,14 @@ class Brain1:
             angle_error = np.arctan2((self.curr_dest_y - self.current_pos[1]),(self.curr_dest_x - self.current_pos[0])) * 180/pi
             angle_error = angle_error - ((-1)*(self.car_direction) + 180)
 
-            self.steer_flag = self.steering_flag(angle_error)           
+            # self.steer_flag = self.steering_flag(angle_error)           
 
 
-                
+            self.calculation_car_yaw()
+
+            print(self.steer_flag)
             if self.steer_flag == 0:
-                self.steer_forward(self.lidar[90], self.database.car.speed)
+                self.steer_forward(self.lidar[90], self.car_speed)
             elif self.steer_flag == 1:
                 self.steer_right()
             elif self.steer_flag == - 1:
@@ -79,20 +129,12 @@ class Brain1:
             else:
                 self.steer_backward()
             #print(self.database.v2x_data.keys())
-
-
+            
             if self.reach():
                 self.global_path.remove(self.global_path[0])
 
             self.previous_pos = self.current_pos
             self.prev_trophy_x, self.prev_trophy_y = self.trophy_x, self.trophy_y
-
-
-                 
-
-                
-    
-                
 
 
 
@@ -124,7 +166,7 @@ class Brain1:
 
 
     def first_waypoint(self):
-        
+        self.global_path = []
         if self.current_pos[1] >= 400:
             if self.current_pos[0] <= 350:
                 # 4, 2
@@ -192,7 +234,7 @@ class Brain1:
 
     def respawn_car_flag(self):
         dist = (self.previous_pos[0] - self.current_pos[0])**2 + (self.previous_pos[1] - self.current_pos[1])**2
-        if dist > 600000:   # 600000은 실험값
+        if dist > 6000:   # 600000은 실험값
             return True
         else:
             return False
@@ -204,55 +246,59 @@ class Brain1:
             return False
 
 
-            
+    def calculation_car_yaw(self):
+        if self.database.car.direction is not None:
+            if self.database.car.direction <= 90:
+                self.car_yaw = self.database.car.direction
+            elif self.database.car.direction > 90 and self.database.car.direction <= 180:
+                self.car_yaw = self.database.car.direction - 90
+            elif self.database.car.direction > 180 and self.database.car.direction <= 270:
+                self.car_yaw = self.database.car.direction - 180
+            else:
+                self.car_yaw = self.database.car.direction - 270
+
 
     def steer_forward(self, lidar : int, speed : int):
 
-        steering_gain = 1
-
+        steering_gain = 2
 
         front_right = np.average(self.lidar[0:59])
         front_left = np.average(self.lidar[120:179])
         
         self.position = (front_left - front_right) / (front_right + front_left)
-    
-        #if abs(self.position) < 0.1:
-        #     steering_gain = 1
-        # elif abs(self.position) > 0.1 and abs(self.position):
-        #     steering_gain = 2
+        self.degree = self.car_direction
 
-        if self.position > 0:
-             self.left(steering_gain)
-        else:
-             self.right(steering_gain)
 
-        # if self.position > 0:
-        #     self.left(steering_gain)
-        # else:
-        #     self.right(steering_gain)
-
-        if self.lidar[89] < 70 and front_left < front_right:
-            self.degree = self.car_direction
-            
-            self.steer_flag = 1
-        elif self.lidar[89] < 70 and front_left > front_right:
-            self.degree = self.car_direction
-            self.steer_flag = -1
-        elif self.lidar[89] < 70:
-            self.degree = self.car_direction
-            self.steer_flag = -1
-            
-            
-            
-
+        print('forward', self.position, self.car_direction, self.car_yaw % 90)
         if lidar == 100:
             self.velocity_control(15)
         elif lidar < 100:
-            self.velocity_control(7)
-            # if speed > 5:
-            #     self.down(3)
+            self.velocity_control(6)
 
-        # print('forward', self.position, self.database.car.direction, steering_gain)
+        if self.car_yaw % 90 == 0: 
+            print('그냥 직진')
+            pass
+        else:
+
+            if self.position > 0 and self.car_yaw % 90 < 45:
+                self.right(steering_gain)
+            elif self.position > 0 and self.car_yaw % 90 > 45:
+                self.left(steering_gain)
+            elif self.position < 0 and self.car_yaw % 90 < 45:
+                self.right(steering_gain)
+            elif self.position < 0 and self.car_yaw % 90 > 45:
+                self.left(steering_gain)
+            
+
+
+        if self.lidar[89] < 70 and front_left < front_right:
+            self.steer_flag = 1
+        elif self.lidar[89] < 70 and front_left > front_right:
+            self.steer_flag = -1
+ 
+
+
+
 
 
 
@@ -263,8 +309,8 @@ class Brain1:
         pass
 
     def steer_right(self):
-        self.velocity_control(5)
-        # print('right', self.degree, self.database.car.direction, self.database.lidar.data[89])
+        self.velocity_control(6)
+        print('right', self.degree, self.car_direction, self.lidar[89])
         for i in range(8):
             self.right(8)
 
@@ -275,8 +321,8 @@ class Brain1:
 
 
     def steer_left(self):
-        self.velocity_control(5)
-        # print('left', self.degree, self.database.car.direction, self.database.lidar.data[89])
+        self.velocity_control(6)
+        print('left', self.degree, self.car_direction, self.lidar[89])
         for i in range(8):
             self.left(8)
 
@@ -288,7 +334,7 @@ class Brain1:
 
     def velocity_control(self, speed):
         if self.database.car.speed >= speed:
-            self.down(2)
+            self.down(10)
         else:
             self.up()
 
